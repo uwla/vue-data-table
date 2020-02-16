c59a17b88f6fc0328477610447d71b63795b8451
uwla <uwlalinux@gmail.com>
2020-02-16 17:45:33 -0300

    add pagination

--- src/components/DataTable/Pagination/DataTablePagination.js
+++ src/components/DataTable/Pagination/DataTablePagination.js
@@ -1,10 +1,48 @@
 export default {
     name: "DataTablePagination",
 
+    computed: {
+        isLastPage() {
+            return this.currentPage == this.numberOfPages;
+        },
+
+        isFirstPage() {
+            return this.currentPage == 1;
+        },
+
+        previousPage() {
+            return this.currentPage - 1;
+        },
+
+        nextPage() {
+            return this.currentPage + 1;
+        },
+    },
+
     props: {
-        params: {
-            type: Object,
+        numberOfPages: {
+            type: Number,
+            required: true,
+        },
+        currentPage: {
+            type: Number,
+            required: true,
+        },
+        nextButtonText: {
+            type: String,
+            required: true,
+        },
+        previousButtonText: {
+            type: String,
             required: true,
         }
     },
+
+    methods: {
+        setCurrentPage(pageNumber) {
+            if (pageNumber != this.currentPage && pageNumber > 0 && pageNumber <= this.numberOfPages) {
+                this.$emit('pageClicked', pageNumber);
+            }
+        }
+    }
 };
--- src/components/DataTable/Pagination/DataTablePagination.vue
+++ src/components/DataTable/Pagination/DataTablePagination.vue
@@ -1,8 +1,36 @@
 <template>
     <div>
-        PAGINATION
+        <ul class="pagination">
+            <li @click="setCurrentPage(previousPage)"
+                class="page-item" :class="{disabled: isFirstPage}">
+
+                <span class="page-link">
+                    {{ previousButtonText }}
+                </span>
+            </li>
+            <!--
+                // 7 -> display all : 1 2 3 4 5 6 7
+                // 8 -> 1 2 3 4 5 ... 8
+                // 9 -> 1 ... 4 5 6 ... 9
+             -->
+            <li v-for="i in numberOfPages" :key="i" @click="setCurrentPage(i)"
+                class="page-item" :class="{disabled: currentPage == i}">
+
+                <span class="page-link" >
+                    {{ i }}
+                </span>
+            </li>
+
+            <li @click="setCurrentPage(nextPage)"
+                class="page-item" :class="{disabled: isLastPage}">
+
+                <span class="page-link">
+                    {{ nextButtonText }}
+                </span>
+            </li>
+        </ul>
     </div>
 </template>
 
-
 <script src="./DataTablePagination.js"></script>
+<style src="./DataTablePagination.scss" lang="scss"></style>
