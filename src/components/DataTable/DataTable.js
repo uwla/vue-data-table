42bba440c74e87a56b65fba9fa014719bee294b1
Andre Souza Abreu <estudantedeciencia@gmail.com>
2020-02-22 17:27:54 -0300

    clean the code: add comments, reorganize functions

--- src/components/DataTable/DataTable.js
+++ src/components/DataTable/DataTable.js
@@ -14,14 +14,6 @@ export const DataTable = {
 
     computed: {
         /**
-         * Merge the parameters passed as props with the default parameters
-         * @return Object
-         */
-        params() {
-            return {...defaultParameters, ...this.parameters};
-        },
-
-        /**
          * Merge the columns passed as props with the default options
          * @return Array
          */
@@ -36,14 +28,62 @@ export const DataTable = {
             })
         },
 
-        searchableColumns() {
-            return this.columns.filter(col => col.searchable)
+        /**
+         * get the data to be displayed
+         * @return Array
+         */
+        data() {
+            let data = this.sortData(this.filteredData);
+            data = this.filterDataByEntryLength(data);
+            return data;
         },
 
-        numberOfPages() {
-            return Math.ceil(this.filteredData.length / this.entryLength) || 1
+        /*
+         * Get the number of records being displayed per page
+         * @return Integer
+         */
+        entryLength() {
+            // get the current entry length
+            if (this.currentEntryLength != null) {
+                return this.currentEntryLength
+            }
+
+            // if the current entry length has not been set
+            // we will return the a default value
+            let {defaultLength} = defaultParameters.entries;
+
+            // the user may have passed a default length
+            // in that case, we need to verify if he/she
+            // passed the entries options in the parameters
+            let {entries} = this.parameters
+
+            if (entries) {
+                if (entries.defaultLength) {
+                    defaultLength = entries.defaultLength;
+
+                // if the user provided an array of possible
+                // lengths, we will return the first one as
+                // the default length
+                } else if (entries.lengths) {
+                    defaultLength = entries.lengths[0]
+                }
+            }
+
+            return defaultLength
         },
 
+        /*
+         * Get the data after filtering it
+         * @return Object
+         */
+        filteredData() {
+            return this.filterDataBySearch(this.params.data);
+        },
+
+        /*
+         * Get the parameters for the EntriesInfo componennt
+         * @return Object
+         */
         infoParams() {
             let totalEntries = this.params.data.length;
             let firstEntry = this.entryLength * (this.currentPage - 1) + 1;
@@ -59,140 +99,76 @@ export const DataTable = {
             return {firstEntry, lastEntry, totalEntries, filteredEntries, text, textFiltered}
         },
 
-        tableWrapperParams() {
-            let {data, columns} = this, {table, tableWrapper, emptyTableText} = this.params;
-            return {data, columns, table, tableWrapper, emptyTableText}
+        /**
+         * Get the number of pages
+         * @return Integer
+         */
+        numberOfPages() {
+            return Math.ceil(this.filteredData.length / this.entryLength) || 1
         },
 
+        /*
+         * Get the parameters for the Pagination componennt
+         * @return Object
+         */
         paginationParams() {
             let {numberOfPages, currentPage, params} = this;
             return {numberOfPages, currentPage, ...params.pagination}
         },
 
         /**
-         * get the data
+         * Merge the parameters passed as props with the default parameters
+         * @return Object
+         */
+        params() {
+            return {...defaultParameters, ...this.parameters};
+        },
+
+        /**
+         * Get columns that can be searched
          * @return Array
          */
-        data() {
-            let data = this.sortData(this.filteredData);
-            data = this.filterDataByEntryLength(data);
-
-            return data;
+        searchableColumns() {
+            return this.columns.filter(col => col.searchable)
         },
 
-        filteredData() {
-            return this.filterDataBySearch(this.params.data);
-        },
-
-        /**
-         * Get the HTML attributes of the table
+        /*
+         * Get the parameters for the tableWrapper componennt
          * @return Object
          */
-        tableAttributes() {
-            //let params = this.params;
-            return {...this.params.tableAttributes, class: this.params.tableClass}
+        tableWrapperParams() {
+            let {data, columns} = this, {table, tableWrapper, emptyTableText} = this.params;
+            return {data, columns, table, tableWrapper, emptyTableText}
         },
 
-        /**
-         * Get the HTML attributes of the table's wrapper
-         * @return Object
-         */
-        tableWrapperAttributes() {
-            let {tableMaxHeight: maxHeight, tableMaxWidth: maxWidth} = this.params;
-
-            return {
-                ...this.params.tableWrapperAttributes, style: {maxHeight, maxWidth}
-            }
-        },
-
-        entryLength() {
-            if (this.currentEntryLength != null) {
-                return this.currentEntryLength
-            }
-
-            let {defaultLength} = defaultParameters.entries;
-            let {entries} = this.parameters
-
-            if (entries) {
-                if (entries.defaultLength) {
-                    defaultLength = entries.defaultLength;
-                } else if (entries.lengths) {
-                    defaultLength = entries.lengths[0]
-                }
-            }
-
-            return defaultLength
-        }
     },
 
     data() {
+        let components = this.parameters.components || defaultParameters.components;
+
         return {
-            sortingColumns: [],
+            sortingColumns: [], // which columns are being sorted
             searchText: "",
             currentPage: 1,
             currentEntryLength: null,
 
-            ...defaultParameters.components.reduce((obj, componentName) => {
-                let key = "show" + componentName;
-                let components = this.parameters.components || defaultParameters.components;
-                obj[key] = components.includes(componentName);
-
-                return obj
-            }, {})
+            // which components to show
+            showEntriesInfo: components.includes("EntriesInfo"),
+            showPagination: components.includes("Pagination"),
+            showEntriesLength: components.includes("EntriesLength"),
+            showSearchFilter: components.includes("SearchFilter"),
         }
     },
 
     methods: {
-        toggleFiltering() {
-            let {value} = window.event.target;
-            this.searchText = value.trim();
-            this.currentPage = 1;
-        },
-
-        toggleSorting(column) {
-            if (!column.orderable) {
-                return;
-            }
-
-            // set the sorting direction
-            if (column.attributes["data-sorting"] == null) {
-                column.attributes["data-sorting"] = "asc";
-
-                // add it to our array
-                column.sortIndex = this.sortingColumns.length;
-                this.sortingColumns.push(column);
-                return;
-            }
-
-            // toggle the sorting direction
-            if (column.attributes["data-sorting"] == "asc") {
-                column.attributes["data-sorting"] = "desc";
-                return;
-            }
-
-            // remove it from sorting
-            column.attributes["data-sorting"] = null;
-            column.sortIndex = null;
-
-            // reset the index
-            this.sortingColumns =
-                this.sortingColumns.filter(col => col.index != column.index).map((col, i) => {
-                    col.sortIndex = i;
-                    return col;
-                });
-        },
-
-
-        /**
-         * Toggle entry length, but still shows the records from the last entry length
-         * @return void
+        /*
+         * Filter the data by the current entry length and the current page.
+         * @return Object
          */
-        toggleEntryLength() {
-            let newEntryLength = Number(window.event.target.value);
-            let firstDataIndex = 1 + this.entryLength * (this.currentPage - 1);
-
-            this.currentEntryLength = newEntryLength;
-            this.currentPage = Math.ceil(firstDataIndex / newEntryLength);
+        filterDataByEntryLength(data) {
+            let start = this.entryLength * (this.currentPage - 1);
+            let end = start + this.entryLength;
+            return data.slice(start, end);
         },
 
         /**
@@ -220,15 +196,17 @@ export const DataTable = {
             });
         },
 
-        filterDataByEntryLength(data) {
-            let start = this.entryLength * (this.currentPage - 1);
-            let end = start + this.entryLength;
-            return data.slice(start, end);
+        /*
+         * Set the current page of the pagination
+         * @return Object
+         */
+        setCurrentPage(page) {
+            this.currentPage = page
         },
 
         /**
          * Perform a case-insensitive sorting.
-         * @return void
+         * @return array
          */
         sortData(data) {
             if (this.sortingColumns.length == 0) {
@@ -238,7 +216,7 @@ export const DataTable = {
             // create a copy of the data
             data = [...data]
 
-            // get the columns to sort
+            // get the columns to perform a multiple sort
             let columns = this.sortingColumns.map(col => {
                 return {key: col.data, direction: col.attributes["data-sorting"]};
             });
@@ -268,8 +246,69 @@ export const DataTable = {
             return data;
         },
 
-        setCurrentPage(page) {
-            this.currentPage = page
+        /**
+         * Toggle entry length, but still shows the records from the last entry length
+         * @return void
+         */
+        toggleEntryLength() {
+            let newEntryLength = Number(window.event.target.value);
+            let firstDataIndex = 1 + this.entryLength * (this.currentPage - 1);
+
+            this.currentEntryLength = newEntryLength;
+            this.currentPage = Math.ceil(firstDataIndex / newEntryLength);
+        },
+
+        /*
+         * Toggle the text used to filter data
+         * @return void
+         */
+        toggleFiltering() {
+            let {value} = window.event.target;
+            this.searchText = value.trim();
+
+            // reset the page to avoid bugs
+            this.currentPage = 1;
+        },
+
+        /*
+         * Toggle the direction of the column being sorted
+         * @return void
+         */
+        toggleSorting(column) {
+            if (!column.orderable) {
+                return;
+            }
+
+            // set the sorting direction if none is set
+            if (column.attributes["data-sorting"] == null) {
+                column.attributes["data-sorting"] = "asc";
+
+                // add it to our array of columns being sorted
+                column.sortIndex = this.sortingColumns.length;
+                this.sortingColumns.push(column);
+                return;
+            }
+
+            // toggle the sorting direction
+            if (column.attributes["data-sorting"] == "asc") {
+                column.attributes["data-sorting"] = "desc";
+                return;
+            }
+
+            // remove it from sorting
+            column.attributes["data-sorting"] = null;
+            column.sortIndex = null;
+
+            // reset the sortIndex of all columns, which indicate
+            // the priority of each column in the sorting. The lower
+            // the value, the higher the priority in the sorting.
+            // This number is displayed on the right side of the
+            // column's title
+            this.sortingColumns =
+                this.sortingColumns.filter(col => col.index != column.index).map((col, i) => {
+                    col.sortIndex = i;
+                    return col;
+                });
         },
     },
 
