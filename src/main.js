1253561d62a3557ff285060f84c36f9cc7dc5e5f
uwla <uwlalinux@gmail.com>
2020-02-22 16:56:46 -0300

    remove jquery and popperjs from dependencies

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import router from './router'
 import store from './store'
 import DataTable from './components/DataTable/DataTable.vue';
 
-//
-import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
