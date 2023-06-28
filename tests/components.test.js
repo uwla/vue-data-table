7e671f209726f8d17ca70ab2149fb6c1e48f05db
uwla <uwlalinux@gmail.com>
2023-06-28 08:03:06 -0300

    add integration tests for pagination

--- tests/components.test.js
+++ tests/components.test.js
@@ -198,6 +198,68 @@ test('it sets correct per page sizes', async () => {
     }
 })
 
+// the per page for the pagination tests
+const perPage = 25
+
+test('it changes pages by clicking on buttons', async () => {
+    await wrapper.setData({ currentPerPage: perPage })
+
+    // the number of buttons may vary depending on the page,
+    // that's why we need to use 'let'
+    let buttons = wrapper.findAll('.vdt-pagination .vdt-page-item')
+    let l = buttons.length
+    let nextBtn = buttons.at(0)
+    let prevBtn = buttons.at(l-1)
+
+    // assert we are in the first page
+    testRowsMatchData(data.slice(0, perPage))
+
+    // go to the next page
+    await nextBtn.trigger('click')
+    testRowsMatchData(data.slice(perPage, perPage*2))
+
+    // go to the next page again
+    await nextBtn.trigger('click')
+    testRowsMatchData(data.slice(perPage*2, perPage*3))
+
+    // go back one page
+    await prevBtn.trigger('click')
+    testRowsMatchData(data.slice(perPage, perPage*2))
+
+    // first entry of the last page
+    let lastPage = Math.ceil(n/perPage)
+    let firstEntry = (lastPage - 1) * perPage
+
+    // go to last page by clicking on the last page button
+    await buttons.at(l-2).trigger('click')
+    testRowsMatchData(data.slice(firstEntry, firstEntry + perPage))
+
+    // go to first page by clicking on the first button
+    await buttons.at(1).trigger('click')
+    testRowsMatchData(data.slice(0, perPage))
+
+    // go to the third page by clicking the 'third-page' button
+    await buttons.at(2).trigger('click')
+    testRowsMatchData(data.slice(perPage*2, perPage*3))
+
+})
+
+test('it changes pages by setting current page', async () => {
+    const paginationInput = wrapper.find('.vdt-pagination input')
+    const lastPage = paginationInput.attributes('max')
+
+    for (let i = 1; i <= lastPage; i+=1) {
+        await paginationInput.setValue(i)
+        let end = perPage*i
+        let start = end - perPage
+        testRowsMatchData(data.slice(start, end))
+    }
+})
+
+// test('it shows the correct number of entries', async () => {
+//
+// })
+
 // TODO:
 //  - test sorting data
 //  - test filtering data
