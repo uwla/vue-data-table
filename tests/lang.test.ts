f490c84c9f0eeab187bf0bfa3808cb277f2c8423
uwla <uwlalinux@gmail.com>
2023-09-28 18:09:27 -0300

    add lang test

---
+++ tests/lang.test.ts
@@ -0,0 +1,46 @@
+import { mount } from '@vue/test-utils'
+import { translations } from '../src/lang'
+import VueDataTable from '../src/components/DataTable.vue'
+
+
+test('test table text matches language', function() {
+
+    const text2cssSelector = {
+        downloadButtonText: ".vdt-export button",
+        downloadText: ".vdt-export span",
+        emptyTableText: ".vdt-empty-body",
+        // infoFilteredText: ".vdt-info",
+        infoText: ".vdt-info",
+        nextButtonText: ".vdt-page-item:last-child",
+        paginationSearchButtonText: ".vdt-pagination-search button",
+        paginationSearchText: ".vdt-pagination-search span",
+        // perPageText: ".vdt-perpage",
+        previousButtonText: ".vdt-page-item:first-child",
+        searchText: ".vdt-search span",
+    } as any
+
+    // text each language
+    for (let lang in translations)
+    {
+        let translation = translations[lang]
+        let wrapper = mount(VueDataTable, {
+            props: {
+                data: [],
+                columnKeys: [],
+                lang: lang,
+            }
+        })
+
+        for (let textKey in text2cssSelector)
+        {
+            let selector = text2cssSelector[textKey]
+            let text = translation[textKey as LanguageDictKey] as string
+
+            // some text have placeholders for the number of rows in the table,
+            // which in this case is zero
+            text = text.replace(/:(last|first|total|entries)/g, '0')
+
+            expect(wrapper.find(selector).text()).toBe(text)
+        }
+    }
+})
