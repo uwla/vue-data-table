5c2bf63ba5bb26003b544e024668ef1cdc6b986e
uwla <uwlalinux@gmail.com>
2023-10-30 10:02:54 -0300

    fix TS7016 error

--- main.d.ts
+++
@@ -1,8 +0,0 @@
-declare const components: {
-    [key: string]: any;
-};
-declare function install(app: any): void;
-declare const plugin: {
-    install: typeof install;
-};
-export { components, plugin, plugin as default, };
