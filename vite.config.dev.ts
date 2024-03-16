62d15ed76bbe281459e57218897905fa077c3224
uwla <uwlalinux@gmail.com>
2024-03-16 09:19:19 -0300

    chore: add npm script to build demo app

--- vite.config.dev.ts
+++ vite.config.dev.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const basePath = process.env.APP_BASE ?? "/";
 
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [vue()],
+    base: basePath,
+    build: {
+        outDir: "demo",
+    },
+});
