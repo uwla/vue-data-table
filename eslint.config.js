f3d13fc049a8df0cbfef0d5f445c10ee2667d1e1
uwla <uwlalinux@gmail.com>
2025-02-19 15:31:58 -0300

    chore: set up eslint

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import eslint from "@eslint/js"
+import eslintConfigPrettier from "eslint-config-prettier"
+import eslintPluginVue from "eslint-plugin-vue"
+import globals from "globals"
+import typescriptEslint from "typescript-eslint"
+
+export default typescriptEslint.config(
+    {
+        ignores: [
+            "*.d.ts",
+            "**/coverage",
+            "**/dist",
+            "assets/",
+            "src/dev.ts",
+        ],
+    },
+    {
+        extends: [
+            eslint.configs.recommended,
+            ...typescriptEslint.configs.recommended,
+            ...eslintPluginVue.configs["flat/recommended"],
+        ],
+        files: ["**/*.{ts,vue}"],
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: globals.browser,
+            parserOptions: { parser: typescriptEslint.parser },
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "off", // Allow the use of `any`
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    args: "all",
+                    argsIgnorePattern: "^_",
+                    varsIgnorePattern: "^_",
+                    caughtErrorsIgnorePattern: "^_",
+                },
+            ],
+            "vue/multi-word-component-names": "off", // Disable multi-word component names rule
+            "vue/require-default-prop": "off", // Disable requiring default props
+            "vue/no-v-html": "off", // Allow v-html usage (if needed)
+        },
+    },
+    eslintConfigPrettier
+)
